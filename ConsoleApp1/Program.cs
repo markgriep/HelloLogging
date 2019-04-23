7c44f143eecf83a51401ff9ec54f67e6155a8744
Mark Griep <mgriep@summit-mortgage.com>
2019-04-23 16:14:33 -0500

    Virtually the simplest Log4net example. 1)  Added the nuget package (install-package log4net) 2)  Added configuration in the app.config 3)  Added the assembly to the Program.cs file 4)  Added the line that creates the static log object 5)  Logged a simple message with the log.Debug() command

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace ConsoleApp1
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program.cs");
+
         static void Main(string[] args)
         {
+            log.Debug("log a message");
             Console.WriteLine("Press any key to exit");
-            Console.ReadKey(); 
+            Console.ReadKey();
         }
     }
 }
