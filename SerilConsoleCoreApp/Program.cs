99c0788022781265ff2742a03e91cc42ef07602b
Mark Griep <mgriep@summit-mortgage.com>
2019-04-26 14:38:23 -0500

    updated packages Added Core application

---
+++ SerilConsoleCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerilConsoleCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
