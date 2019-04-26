62a09110870e4f98003e58f6547f5167a5678666
Mark Griep <mgriep@summit-mortgage.com>
2019-04-26 14:54:31 -0500

    serilog simple logging

--- SerilConsoleCoreApp/Program.cs
+++ SerilConsoleCoreApp/Program.cs
@@ -7,42 +7,24 @@ namespace SerilConsoleCoreApp
     {
         static void Main(string[] args)
         {
-
             var log = new LoggerConfiguration()
-
-               .WriteTo.ColoredConsole(outputTemplate: " {Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
-               .MinimumLevel.Verbose()
-
-               .WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)  // 
-
-               .WriteTo.File("log/Xog.txt", retainedFileCountLimit: 4, rollingInterval: RollingInterval.Minute)
-
-               .WriteTo.File("log/Yog.txt", fileSizeLimitBytes: 1024, retainedFileCountLimit: 5, rollOnFileSizeLimit: true)
-
-              
-
-               .CreateLogger();
+                .WriteTo.Console()
+                .WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)
+                .MinimumLevel.Verbose()
+                .CreateLogger();
 
             var userId = "mgasrs";
-            log.Debug($"User id is: {userId}");
+
             log.Fatal("this is fatal");             // Highest
             log.Error("this is error");
             log.Warning("this is warning");
             log.Information("this is info");
-            log.Debug("this is debug");
+            log.Debug($"debug from : {userId}");
             log.Verbose("this is verbose");         // Lowest
 
 
             Console.ReadKey();
 
-
-            Console.ReadKey();
-
-
-
-
-
-            Console.WriteLine("Hello Core World!");
         }
     }
 }
