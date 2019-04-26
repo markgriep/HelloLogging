a915b1f35a905a0715e37f8021cd129b41959ee1
Mark Griep <mgriep@summit-mortgage.com>
2019-04-26 14:41:55 -0500

    Serilog works virtually the same way with .net core

--- SerilConsoleCoreApp/Program.cs
+++ SerilConsoleCoreApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 
 namespace SerilConsoleCoreApp
 {
@@ -6,7 +7,42 @@ namespace SerilConsoleCoreApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var log = new LoggerConfiguration()
+
+               .WriteTo.ColoredConsole(outputTemplate: " {Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
+               .MinimumLevel.Verbose()
+
+               .WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)  // 
+
+               .WriteTo.File("log/Xog.txt", retainedFileCountLimit: 4, rollingInterval: RollingInterval.Minute)
+
+               .WriteTo.File("log/Yog.txt", fileSizeLimitBytes: 1024, retainedFileCountLimit: 5, rollOnFileSizeLimit: true)
+
+              
+
+               .CreateLogger();
+
+            var userId = "mgasrs";
+            log.Debug($"User id is: {userId}");
+            log.Fatal("this is fatal");             // Highest
+            log.Error("this is error");
+            log.Warning("this is warning");
+            log.Information("this is info");
+            log.Debug("this is debug");
+            log.Verbose("this is verbose");         // Lowest
+
+
+            Console.ReadKey();
+
+
+            Console.ReadKey();
+
+
+
+
+
+            Console.WriteLine("Hello Core World!");
         }
     }
 }
