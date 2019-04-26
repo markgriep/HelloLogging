ae028be15b709d553a7b898f94f89eed81307b5a
Mark Griep <mgriep@summit-mortgage.com>
2019-04-26 10:59:44 -0500

    Added simple console logging

--- SerilogConsoleApp/Program.cs
+++ SerilogConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,15 @@ namespace SerilogConsoleApp
     {
         static void Main(string[] args)
         {
+            var log = new LoggerConfiguration()
+               .WriteTo.Console()
+               .CreateLogger();
+
+            log.Information("Hello, Serilog!");
+
+
+            Console.ReadKey();
+
         }
     }
 }
