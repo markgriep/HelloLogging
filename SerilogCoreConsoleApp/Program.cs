6ea2de712e1427bc3a631023663da68e7bffc69c
Mark Griep <markgriep@users.noreply.github.com>
2024-02-06 11:17:19 -0600

    added experiments with different roll options.

--- SerilogCoreConsoleApp/Program.cs
+++ SerilogCoreConsoleApp/Program.cs
@@ -11,22 +11,47 @@ namespace SerilogCoreConsoleApp
 
             var log = new LoggerConfiguration()
 
-              .WriteTo.Console(outputTemplate: " {Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
+              .WriteTo.Console(outputTemplate: " {Timestamp:HH:mm:ss} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
          
               .MinimumLevel.Verbose()
 
-              .WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)  // 
+              .WriteTo.File("log/RollingDaylog.txt", rollingInterval: RollingInterval.Day)  // 
 
-              .WriteTo.File("log/Xog.txt", retainedFileCountLimit: 4, rollingInterval: RollingInterval.Minute)
+              .WriteTo.File("log/RollingMinuteLog.txt", retainedFileCountLimit: 4, rollingInterval: RollingInterval.Minute)
 
-              .WriteTo.File("log/Yog.txt", fileSizeLimitBytes: 1024, retainedFileCountLimit: 5, rollOnFileSizeLimit: true)
+              .WriteTo.File("log/Size-NumberLimitLog.txt", fileSizeLimitBytes: 1024, retainedFileCountLimit: 10, rollOnFileSizeLimit: true)
 
               //.WriteTo.SQLite("db/test.db", tableName: "dataLog")
 
               .CreateLogger();
 
-            var userId = "mgasrs";
-            log.Debug($"User id is: {userId}");
+
+            for (int i = 0; i < 1990; i++)
+            {
+            WriteLogStuff(log, i);
+
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+    
+
+
+
+    public static void WriteLogStuff(ILogger log, int x)
+        {
+
+            string? proc = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            string? userId = Environment.GetEnvironmentVariable("USERNAME");
+            string? ComputerName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+            
+            log.Debug($"===== Loop #: {x}");
+            log.Debug($"     User ID: {userId}");
+            log.Debug($"         CPU: {proc}");
+            log.Debug($"    Computer: {ComputerName}");
             log.Fatal("this is fatal");             // Highest
             log.Error("this is error");
             log.Warning("this is warning");
@@ -35,8 +60,7 @@ namespace SerilogCoreConsoleApp
             log.Verbose("this is verbose");         // Lowest
 
 
-            Console.ReadKey();
-
         }
+
     }
 }
