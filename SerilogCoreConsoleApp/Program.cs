95f501c278e7d2da1b47a937460cbd05e84ce4ec
Mark Griep <markgriep@users.noreply.github.com>
2024-02-06 14:30:11 -0600

    added some randomeness methods

--- SerilogCoreConsoleApp/Program.cs
+++ SerilogCoreConsoleApp/Program.cs
@@ -10,8 +10,6 @@ namespace SerilogCoreConsoleApp
         static void Main(string[] args)
         {
 
-
-
             //var log = new LoggerConfiguration()
             //  .WriteTo.Console(outputTemplate: " {Timestamp:HH:mm:ss} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
             //  .MinimumLevel.Verbose()
@@ -24,33 +22,20 @@ namespace SerilogCoreConsoleApp
 
             Console.WriteLine(  Directory.GetCurrentDirectory());
 
-
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
-
-
 
             var log = new LoggerConfiguration()
                 .ReadFrom.Configuration(config)
                 .CreateLogger();
 
-
-
-
-
             for (int i = 0; i < 199; i++)
             {
-
-
-            WriteLogStuff(log, i);
-
+                WriteLogStuff(log, i);
             }
 
-
-
             Console.ReadKey();
-
         }
 
 
@@ -58,7 +43,7 @@ namespace SerilogCoreConsoleApp
 
     public static void WriteLogStuff(ILogger log, int x)
         {
-            Thread.Sleep(new Random().Next(100, 7000));
+            Thread.Sleep(GetBellRandom(100, 700, 500));
 
             string? proc = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
             string? userId = Environment.GetEnvironmentVariable("USERNAME");
@@ -78,5 +63,22 @@ namespace SerilogCoreConsoleApp
 
         }
 
+    public static int GetBellRandom(int min, int max, int mean)
+    {
+            double stdDev = (max - min) / 6.0; // Assuming a standard deviation where ~99.7% of values fall within min and max
+            Random rand = new Random();
+            double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - rand.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            double randNormal = mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+            return Math.Max(min, Math.Min(max, (int)Math.Round(randNormal))); // Clipping to min and max
     }
+
+
+
+
+
+
+
+}
 }
